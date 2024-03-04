29a8189280ead7152da27436b6ac0a668b0698a4
yeswinsompalli <156709362+yeswinsompalli@users.noreply.github.com>
2024-03-03 20:34:07 -0500

    Add files via upload

---
+++ Feature-B Subtask-1.py
@@ -0,0 +1,6 @@
+class TeamMember:
+    def __init__(self, name, days_off, hours_committed_to_ceremonies, available_hours_range):
+        self.name = name
+        self.days_off = days_off
+        self.hours_committed_to_ceremonies = hours_committed_to_ceremonies
+        self.available_hours_range = available_hours_range
