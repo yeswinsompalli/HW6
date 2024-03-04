6fc62ed859091e0e0444bcd9d1c5b9e2e124675e
yeswinsompalli <156709362+yeswinsompalli@users.noreply.github.com>
2024-03-03 21:13:34 -0500

    Add files via upload

--- Feature B Subtask-4.py
+++ Feature B Subtask-4.py
@@ -1,3 +1,7 @@
+    def calculate_team_capacity(self, sprint_days):
+        total_effort_hours = sum(member.calculate_available_hours(sprint_days) for member in self.team_members)
+        return total_effort_hours
+
     def print_individual_and_team_capacity(self, sprint_days):
         for member in self.team_members:
             print(f"{member.name} Available Effort-Hours: {member.calculate_available_hours(sprint_days)}")
